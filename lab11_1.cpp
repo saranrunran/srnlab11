096402629626f8fba070633215984f6ee636774a
saranrunran <peemai.saranporn@gmail.com>
2025-12-26 15:08:23 +0700

    name_filled

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+
+
+int main() {
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin << 
+}
